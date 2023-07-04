9e015d8173e678b2ff169ac24ecf9c8292c9453f
tangxf <547369334@qq.com>
2023-07-04 16:31:25 +0800

    fix：init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+    // "browser": true,
+    // "es2021": true,
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    // "parser": "vue-eslint-parser",
+    // "parserOptions": {
+    //     "ecmaVersion": "latest",
+    //     "parser": "@typescript-eslint/parser",
+    //     "sourceType": "module"
+    // },
+    "plugins": [
+        "@typescript-eslint",
+        "vue"
+    ],
+    "rules": {
+    }
+}
